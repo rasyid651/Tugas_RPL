0d0d6d1a149a3816da1eb39fb184558423e47153
rasyid651 <muhammadalrasyid789@gmail.com>
2025-09-15 10:37:28 +0700

    first

---
+++ menu_makanan.java
@@ -0,0 +1,27 @@
+
+package projek_besar;
+
+import java.util.Scanner;
+
+public class menu_makanan {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        //variabel untuk menyimpan hasil pesanan
+        int totalHarga = 0;
+        int totalPorsi = 0;
+        String daftarPesanan = "";
+        
+        //daftar menu
+        while (true) {
+        
+            System.out.println("\n === Menu Makanan === ");
+            System.out.println("1. Ayam - Rp10.000");
+            System.out.println("2. Ikan - Rp.15.000");
+            System.out.println("3. sayir - Rp.200");
+        }
+    }
+    
+}
